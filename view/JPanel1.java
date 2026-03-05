9db580d975f320a000fe7e5e71861b114541fd56
Jose Ramon Neira Vega <jramonnv0786@gmail.com>
2026-03-05 19:20:40 +0100

    Cambios estructura de clase de BancoDePreguntas

--- view/JPanel1.java
+++ view/JPanel1.java
@@ -1,7 +1,29 @@
 package view;
 
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 public class JPanel1 extends JPanel {
     
+    //Declaración de los componentes de la interfaz gráfica
+    private JLabel labelCategoria;
+    private JTextArea txtPregunta;
+    private JButton btnOpcion1;
+    private JButton btnOpcion2;
+    private JButton btnOpcion3;
+    private JButton btnOpcion4;
+
+    public JPanel1() {
+        // Al crear el panel, inicializamos la interfaz
+        inicializarComponentes();
+    }
+
+// Método para inicializar los componentes de la interfaz gráfica
+    private void inicializarComponentes() {
+        //Configuración del panel principal layout y márgenes
+        this.setLayout(new BorderLayout(20, 20));
+        this.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        this.setBackground(new Color(245, 245, 245)); // Un gris muy claro de fondo
+    }
+
 }
